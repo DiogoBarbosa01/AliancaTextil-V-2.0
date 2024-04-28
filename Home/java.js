4910271fe0fbb987f045493124cfd1f6c4ca70b7
NicolasG0ulart <nicolasgoulart5001@gmail.com>
2024-04-28 09:39:26 -0300

    ajustes na header e section sobre nos

---
+++ Home/java.js
@@ -0,0 +1,11 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(nextSlide, 10000); // Troca de slide a cada 7 segundos (ajuste conforme necessário)
+
